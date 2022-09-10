76b4f84dda346522c3711353fb8bb9bf77b895f5
JudeJeanPierre <110211488+JudeJeanPierre@users.noreply.github.com>
2022-09-09 22:55:38 -0500

    4th commit

--- wordguess.js
+++ wordguess.js
@@ -1,189 +1,209 @@
 const words = [
     {
-    word = "location",
-    hint = "Position of someone or something (French translation: Localisation)"
+    word : "location",
+    hint : "Position of someone or something (French translation: Position)"
 },
 {
-    word ="school",
-    hint = "... (French translation:...)"
+    word : "school",
+    hint : "... (French translation:...)"
 },
 {
-    word = "alumni",
-    hint = "... (French translation:...)"
+    word : "alumni",
+    hint : "... (French translation:...)"
 },
 {
-    word = "restaurant",
-    hint = "... (French translation:...)"
+    word : "restaurant",
+    hint : "... (French translation:...)"
 },
 {
-    word = "hospital",
-    hint = "... (French translation:...)"
+    word : "hospital",
+    hint : "... (French translation:...)"
 },
 {
-    word = "garden",
-    hint = "... (French translation:...)"
+    word : "garden",
+    hint : "... (French translation:...)"
 },
 {
-    word = "thanks",
-    hint = "... (French translation:...)"
+    word : "thanks",
+    hint : "... (French translation:...)"
 },
 {
-    word = "morning",
-    hint = "... (French translation:...)"
+    word : "morning",
+    hint : "... (French translation:...)"
 },
 {
-    word = "afternoon",
-    hint = "... (French translation:...)"
+    word : "afternoon",
+    hint : "... (French translation:...)"
 },
 {
-    word = "teacher",
-    hint = "... (French translation:...)"
+    word : "teacher",
+    hint : "... (French translation:...)"
 },
 {
-    word = "classmate",
-    hint = "... (French translation:...)"
+    word : "classmate",
+    hint : "... (French translation:...)"
 },
 {
-    word = "circumstance",
-    hint = "... (French translation:...)"
+    word : "circumstance",
+    hint : "... (French translation:...)"
 },
 {
-    word = "average",
-    hint = "... (French translation:...)"
+    word : "average",
+    hint : "... (French translation:...)"
 },
 {
-    word = "software",
-    hint = "... (French translation:...)"
+    word : "software",
+    hint : "... (French translation:...)"
 },
 {
-    word = "engineer",
-    hint = "... (French translation:...)"
+    word : "engineer",
+    hint : "... (French translation:...)"
 },
 {
-    word = "wedding",
-    hint = "... (French translation: Mariage)"
+    word : "wedding",
+    hint : "... (French translation: Mariage)"
 },
 {
-    word = "employment",
-    hint = "... (French translation: Emploi)"
+    word : "employment",
+    hint : "... (French translation: Emploi)"
 },
 {
-    word = "interview",
-    hint = "... (French translation: Entrevue)"
+    word : "interview",
+    hint : "... (French translation: Entrevue)"
 },
 {
-    word = "employer",
-    hint = "... (French translation: patron)"
+    word : "employer",
+    hint : "... (French translation: patron)"
 },
 {
-    word = "employee",
-    hint = "... (French translation: Employé)"
+    word : "employee",
+    hint : "... (French translation: Employé)"
 },
 {
-    word = "brother",
-    hint = "... (French translation: Frère)"
+    word : "brother",
+    hint : "... (French translation: Frère)"
 },
 {
-    word = "beautiful",
-    hint = "... (French translation: Belle)"
+    word : "beautiful",
+    hint : "... (French translation: Belle)"
 },
 {
-    word = "computer",
-    hint = "... (French translation: Ordinateur)"
+    word : "computer",
+    hint : "... (French translation: Ordinateur)"
 },
 {
-    word = "library",
-    hint = "... (French translation: Bibliothèque)"
+    word : "library",
+    hint : "... (French translation: Bibliothèque)"
 },
 {
-    word = "fortunate",
-    hint = "... (French translation: Riche)"
+    word : "fortunate",
+    hint : "... (French translation: Riche)"
 },
 {
-    word = "happy",
-    hint = "... (French translation: Content)"
+    word : "happy",
+    hint : "... (French translation: Content)"
 },
 {
-    word = "notebook",
-    hint = "... (French translation: Cahier)"
+    word : "notebook",
+    hint : "... (French translation: Cahier)"
 },
 {
-    word = "homework",
-    hint = "... (French translation: devoir de maison)"
+    word : "homework",
+    hint : "... (French translation: devoir de maison)"
 },
 {
-    word = "knowledge",
-    hint = "... (French translation: Connaissance)"
+    word : "knowledge",
+    hint : "... (French translation: Connaissance)"
 },
 {
-    word = "",
-    hint = "... (French translation:...)"
+    word : "",
+    hint : "... (French translation:...)"
 },
 {
-    word = "husband",
-    hint = "... (French translation: Mari, époux)"
+    word : "husband",
+    hint : "... (French translation: Mari, époux)"
 },
 {
-    word = "bedroom",
-    hint = "... (French translation: Chambre à coucher)"
+    word : "bedroom",
+    hint : "... (French translation: Chambre à coucher)"
 },
 {
-    word = "sneaker",
-    hint = "... (French translation: Tennis)"
+    word : "sneaker",
+    hint : "... (French translation: Tennis)"
 },
 {
-    word = "party",
-    hint = "... (French translation: Fête)"
+    word : "party",
+    hint : "... (French translation: Fête)"
 },
 {
-    word = "delivery",
-    hint = "... (French translation: Livraison)"
+    word : "delivery",
+    hint : "... (French translation: Livraison)"
 },
 {
-    word = "network",
-    hint = "... (French translation: Réseau)"
+    word : "network",
+    hint : "... (French translation: Réseau)"
 },
 {
-    word = "congratulations",
-    hint = "... (French translation: Félicitations)"
+    word : "congratulations",
+    hint : "... (French translation: Félicitations)"
 },
 {
-    word:"greetings",
-    hint = "... (French translation: Salutations)"
+    word :"greetings",
+    hint : "... (French translation: Salutations)"
 }
-]
-// Variables for game state
-let player1Score = 0;
-let player2Score = 0;
-let word = '';
-let p1guess = 2;
-let p2guess = 2;
-let timer = 30;
-let player1Turn = true
+];
+
 
 // Variables to store
-const player1Scoreboard = document.getElementById("player1Scoreboard");
-const player2Scoreboard = document.getElementById("player2Scoreboard");
+const score1Element = document.querySelector(".player1Scoreboard span");
+const score2Element = document.querySelector(".player2Scoreboard span");
 const wordElement = document.querySelector(".word");
 const hintElement = document.querySelector(".hint span");
-const startTheGame = document.getElementById("btnStart");
-const confirmWord= document.getElementById("btnConfirm");
-const changeBgColor = document.getElementById("btnBgColor");
-const changeBdColor = document.getElementById("btnBdColor");
-const messageDisplay = document.getElementById("message");
+const startTheGameButton = document.querySelector(".btnStart");
+const confirmWordButton = document.querySelector(".btnConfirm");
+const changeBgColorButton = document.querySelector(".btnBgColor");
+const changeBdColorButton = document.querySelector(".btnBdColor");
+const messageDisplay = document.querySelector(".message");
 const input = document.querySelector("input");
-const timerElement = document.querySelector(".timer span b");
-const p1guessLeft = document.querySelector(".p1guess");
-const p2guessLeft = document.querySelector(".p2guess");
+const timeLeftElement = document.querySelector(".time span");
+const guesses1Left = document.querySelector(".p1guess span");
+const guesses2Left = document.querySelector(".p2guess span");
 // alert(player1Turn);
 
-function setGame(){
-    let randomIndex = Math.floor(Math.random() * words.length);
-    let randomObject = words [randomIndex];
-    word = randomObject.word.toLocaleLowerCase();
-    console.log(randomObject)
+// Variables for game state
+let player1Scoreboard = 0;
+let player2Scoreboard  = 0;
+let word = "";
+let stopwatch; 
+let p1guess = 3;
+let p2guess = 3;
+let time = 20; 
+// let player1Turn = true;
+
+function startGame(){
+  
+    let randomWord = words[Math.floor(Math.random() * words.length)];
+    word = randomWord.word.toLowerCase();
+    hint = randomWord.hint;
+    console.log(word, hint);
 
     let wordArray = word.split("").sort(() => Math.random() - 0.5);
-    let confuseWord = wordArray.join
-    console.log(confuseWord)
+    let confuseWord = wordArray.join("");
+    // return confuseWord
+    
+    
+    if(confuseWord === word)
+    return startGame();
+    
+    guesses1Left.innerText = p1guess;
+    guesses2Left.innerText = p2guess;
+    score1Element.innerText = player1Scoreboard;
+    score2Element.innerText = player2Scoreboard;
+    wordElement.innerText = confuseWord;
+    hintElement.innerText = randomObject.hint;
+    timeLeftElement.innerText = time;
+    input.value = "";
+    btnConfirm.setAttribute("disabled", true);
 }
+
+startGame()
